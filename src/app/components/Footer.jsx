e54ae7c6615cfbc82cb9edd9e230a51342a78b83
YohannDelacroix <yohann.delacroix@hotmail.fr>
2025-01-12 16:11:10 +0100

    Optimized Footer for SEO : Removing use client , and downloading PDF file from public next repository directly

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,78 +1,32 @@
-"use client"
 import "../../styles/footer.css"
 import React from 'react'
-import { useEffect, useState } from "react";
 import axios from "../../api/axios.js";
 import { SiFacebook, SiInstagram, SiYoutube } from 'react-icons/si'
 import { IconContext } from "react-icons";
 
 
 const Footer = () => {
-    const [pressKit, setPressKit] = useState(null);
-
-    useEffect(() => {
-        console.log("GetPressKit");
-        const getPressKit = async () => {
-            try {
-                let pdfFile = await axios.get("/pressKit", { responseType: 'blob' });
-
-                if (pdfFile.data) {
-                    console.log("pdfFile loaded")
-                    let objectURL = window.URL.createObjectURL(pdfFile.data);
-                    setPressKit(objectURL);
-                }
-                else {
-                    console.log("not found")
-                }
-            }
-            catch (err) {
-                console.log(err.message)
-            }
-        }
-
-        getPressKit();
-    }, [])
-
     return (
         <div className="footer">
-
             <div className="footer-contact">
-                {//<p>+33 779 77 40 26</p>
-                }
                 <p>redlightdistortion.metalband@gmail.com</p>
-                <p>  <a href={pressKit} target="_blank" rel="noreferrer"><button>Download Press Kit (FR)</button></a></p>
+                <p>  <a href="/assets/DossierREDLIGHTDISTORTION-1.pdf" target="_blank" rel="noreferrer"><button>Download Press Kit (FR)</button></a></p>
             </div>
 
             <div className="footer-social">
-                {
-                    /* 
-                            <a href="https://www.facebook.com/redlightdistortion/" target="_blank"><img className="footer-social-image" src={LogoFb} alt="f" /></a><a href="https://www.instagram.com/redlightdistortion/" target="_blank"><img className="footer-social-image" src={LogoInsta} alt="f" /></a><a href="https://www.youtube.com/channel/UC1C7_waXIYi6cdy_jCPKoLQ" target="_blank"><img className="footer-social-image" src={LogoYouTube} alt="f" /></a>
-            
-                    */
-                }
                 <a href="https://www.facebook.com/redlightdistortion/" target="_blank" rel="noreferrer">
-                    <IconContext.Provider value={{ className: 'footer-react-icons', size: '25px' }}>
-                        <div>
-                            <SiFacebook />
-                        </div>
+                    <SiFacebook className='footer-react-icons' size='25px' />
 
-                    </IconContext.Provider>
                 </a>
 
                 <a href="https://www.instagram.com/redlightdistortion/" target="_blank" rel="noreferrer">
-                    <IconContext.Provider value={{ className: 'footer-react-icons', size: '25px' }}>
-                        <div><SiInstagram /></div>
-                    </IconContext.Provider>
-
+                    <SiInstagram className='footer-react-icons' size='25px' />
                 </a>
 
                 <a href="https://www.youtube.com/channel/UC1C7_waXIYi6cdy_jCPKoLQ" target="_blank" rel="noreferrer">
-                    <IconContext.Provider value={{ className: 'footer-react-icons', size: '25px' }}>
-                        <div><SiYoutube /></div>
-                    </IconContext.Provider>
+                    <SiYoutube className='footer-react-icons' size='25px' />
                 </a>
             </div>
-
         </div>)
 }
 
