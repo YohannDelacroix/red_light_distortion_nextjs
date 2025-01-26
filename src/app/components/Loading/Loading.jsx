bf4ef5064d905e5fb4b4c66cce7853a2795951f7
YohannDelacroix <yohann.delacroix@hotmail.fr>
2025-01-26 17:33:46 +0100

    Loading component explained in comments, added role and aria attributes for better accessibility

--- src/app/components/Loading/Loading.jsx
+++ src/app/components/Loading/Loading.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import "@/styles/loading.css"
 
+/**
+ * Loading Component
+ * Displays a spinning loader to indicate that a process is in progress.
+ * 
+ * This component uses a CSS-based spinner animation and is designed to be
+ * reusable across different sections of the application.
+ *
+ * Styling:
+ * - The `.loading-container` provides the container for the spinner.
+ * - The `.loading-spin` pseudo-element is used for the spinner animation, 
+ *
+ * Example Usage:
+ * Writing <Loading /> anywhere in the code will display the spinner animation 
+ */
+
 const Loading = () => {
   return (
-    <div className="loading-container">
+    <div className="loading-container" role="status" aria-live="polite">
         <div className="loading-spin"></div>
     </div>
   )
