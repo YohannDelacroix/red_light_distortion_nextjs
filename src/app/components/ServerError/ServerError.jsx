14b9f9f88347f3ef558d21e7dc82f90a2a53d11e
YohannDelacroix <yohann.delacroix@hotmail.fr>
2025-01-27 18:49:34 +0100

    Added comments on ServerError component

--- src/app/components/ServerError/ServerError.jsx
+++ src/app/components/ServerError/ServerError.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 import TitleComponent from '../TitleComponent/TitleComponent'
 import "@/styles/error.css"
 
+
+/**
+ * ServerError Component
+ *
+ * This component displays an error message when the server cannot fulfill 
+ * a request or when an issue occurs on the backend.
+ * It informs users that the issue is being worked on by the admin team.
+ *
+ * @returns {JSX.Element} - A user interface with an error title and message.
+ */
 const ServerError = () => {
   return (
     <div data-testid="server-error" className="error-container">
@@ -13,4 +23,4 @@ const ServerError = () => {
   )
 }
 
-export default ServerError;
+export default ServerError; 
