c6ef7ebd82ece2f72d4156d4ae2d816d818a6930
YohannDelacroix <yohann.delacroix@hotmail.fr>
2025-02-05 12:19:13 +0100

    Performance: used Thumbnail in homepage to increase performance

--- src/app/videos/components/VideoThumbnail.jsx
+++ src/app/videos/components/VideoThumbnail.jsx
@@ -9,41 +9,21 @@ import "@/styles/videos.css"
  * 
  * This component displays a YouTube video thumbnail that, when clicked, 
  * replaces itself with the embedded YouTube player.
- * The component uses IntersectionObserver to load the video iframe only 
- * when it is visible on the screen, improving performance by avoiding unnecessary loading.
  * 
  * Props:
  * @param {string} videoId - The YouTube video ID.
  * @param {string} alt - The alt text for the thumbnail image.
  * @param {string} title - The title of the video.
- * @param {boolean} [autoPlay=false] - Whether the video should autoplay after a delay.
+
  */
 
 
-const VideoThumbnail = ({ videoId, alt, title, autoPlay = false }) => {
+const VideoThumbnail = ({ videoId, alt, title }) => {
     const [isPlaying, setIsPlaying] = useState(false);
-    const videoRef = useRef(null);
-
-    // useEffect hook to handle lazy loading of the video when it becomes visible
-    useEffect(() => {
-        const observer = new IntersectionObserver(
-            ([entry]) => {
-                if (entry.isIntersecting) {
-                    setIsPlaying(true); // Start playing when video is visible
-                    observer.disconnect(); // Stop observing after the video is loaded
-                }
-            },
-            { threshold: 0.5 } // Load the iframe when at least 50% of the video is in the viewport
-        );
-
-        if (videoRef.current) observer.observe(videoRef.current);
-
-        return () => observer.disconnect();
-    }, []);
 
     return isPlaying ? (
         // Render the embedded YouTube video when playing
-        <div ref={videoRef} className="iframe-container">
+        <div className="iframe-container">
             <iframe
                 src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
                 title={title}
@@ -53,7 +33,7 @@ const VideoThumbnail = ({ videoId, alt, title, autoPlay = false }) => {
         </div>
     ) : (
         // Render the video thumbnail with a play button
-        <div ref={videoRef} className='video-thumbnail-container'
+        <div className='video-thumbnail-container'
             onClick={() => setIsPlaying(true)}>
             <div className="video-thumbnail-player">
                 <FaPlay className="video-thumbnail-player-icon" />
