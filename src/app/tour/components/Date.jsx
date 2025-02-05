161a3a76337bf7eec98bafeddbc72bced8f15c6e
YohannDelacroix <yohann.delacroix@hotmail.fr>
2025-02-05 10:55:28 +0100

    Fixed(Date) :
    -The two possibles button are now the same size

--- src/app/tour/components/Date.jsx
+++ src/app/tour/components/Date.jsx
@@ -59,12 +59,12 @@ function Date({ date_content }) {
 
         {/* Render the links if available */}
         <div className="date-links">
-            <div data-testid="more-link" className={dateMore}>
-                <a href={date_content.more_link} target="_blank" rel="noreferrer"><button>More info</button></a>
+            <div data-testid="more-link" className={dateMore}> 
+                <a href={date_content.more_link} target="_blank" rel="noreferrer"><button className="w-100">More info</button></a>
             </div>
 
             <div data-testid="ticket-link" className={dateTicket}>
-                <a href={date_content.ticket_link} target="_blank" rel="noreferrer"><button>Tickets</button></a>
+                <a href={date_content.ticket_link} target="_blank" rel="noreferrer"><button className="w-100">Tickets</button></a>
             </div>
         </div>
     </div>);
